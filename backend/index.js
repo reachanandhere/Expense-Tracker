5d0aaea1c7b00202582274e462c43ccbdf41f578
Anand Verma <anandverma552@gmail.com>
2024-06-20 22:41:35 -0500

    updating bg

--- backend/index.js
+++ backend/index.js
@@ -72,11 +72,11 @@ app.use(
   })
 );
 
-app.use(express.static(path.join(__dirname, "frontend/dist")));
+// app.use(express.static(path.join(__dirname, "frontend/dist")));
 
-app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "frontend/dist", "index.html"));
-});
+// app.get("*", (req, res) => {
+// 	res.sendFile(path.join(__dirname, "frontend/dist", "index.html"));
+// });
 
 
 await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
--- frontend/src/components/pages/HomePage.jsx
+++ frontend/src/components/pages/HomePage.jsx
@@ -117,7 +117,7 @@ const HomePage = ({ userData }) => {
       <div className="flex flex-col gap-6 items-center max-w-7xl mx-auto z-20 relative justify-center">
         <div className="flex items-center">
           <p className="md:text-4xl text-2xl lg:text-4xl font-bold text-center relative z-50 mb-4 mr-4 bg-gradient-to-r from-pink-600 via-indigo-500 to-pink-400 inline-block text-transparent bg-clip-text">
-            Spend wisely, track wisely
+            Spend wisely, track wisely sf
           </p>
           <img
             src={profilePicture}
@@ -140,9 +140,9 @@ const HomePage = ({ userData }) => {
             <Doughnut data={chartData} />
           </div>
 
-          <TransactionForm />
+          {/* <TransactionForm /> */}
         </div>
-        <Cards profilePicture={profilePicture} />
+        {/* <Cards profilePicture={profilePicture} /> */}
       </div>
     </>
   );
